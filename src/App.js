2300f6e7c4749c98b652961b9ed50f0694fc019e
Cristian Buitrago <cbo@misena.edu.co>
2022-03-29 10:20:36 -0500

    styling with props

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from "./componentes/Header"
 function App(){
     return (
         <>
-            <Header  />
+            <Header bgColor='red' textColor='blue' />
                 <div className="container">
                     <h1>My app</h1>   
                 </div>       
