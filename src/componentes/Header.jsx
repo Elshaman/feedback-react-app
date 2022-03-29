5167ac3191bb87a4a4a5ffb278c2bdebafdddf70
Cristian Buitrago <cbo@misena.edu.co>
2022-03-29 10:24:30 -0500

    default styling props

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -19,7 +19,9 @@ function Header({text , bgColor , textColor }) {
 }
 
 Header.defaultProps ={
-    text: 'FeedBack UI'
+    text: 'FeedBack UI',
+    bgColor: 'rgba(0,0,0,0.4)',
+    textColor : '#ff6a95'
 }
 
 Header.propTypes ={
