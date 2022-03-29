410a6033b3b86070a798efbd233f4aad537681ec
Cristian Buitrago <cbo@misena.edu.co>
2022-03-29 00:14:03 -0500

    recorrido de arreglos con map

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 function App(){
+
+    const title = 'Blog Post'
+    const body = 'This is my blog post'
+    const comments = [
+        {id: 1 , text: 'First Comment'},
+        {id: 2 , text: 'Second Comment'},
+        {id: 3 , text: 'Third Comment'},
+    ]
+
+
     return (
-        <>
-            <h1>Hola desde el componente App</h1>
-            <p>Welcome</p>
-        </>
+        <div className="container">
+            <h1>{ title }</h1>
+            <p>{ body }</p>
+            <div className="comments">
+                <h3>Comments ({comments.length})</h3>
+                <ul>
+                    { comments.map( (comment, index) => (
+                        <li key={index}>{ comment.text }</li>
+                    )) }
+                </ul>
+            </div>
+        </div>
        
     )
 }
