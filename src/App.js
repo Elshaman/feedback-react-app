27a75715ad6653dae3a95ed999dd551b146f9020
Cristian Buitrago <cbo@misena.edu.co>
2022-03-28 23:51:23 -0500

    primer fragment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 function App(){
-    return <h1>Hola desde el componente App</h1>
+    return (
+        <>
+            <h1>Hola desde el componente App</h1>
+            <p>Welcome</p>
+        </>
+       
+    )
 }
 
 export default App
