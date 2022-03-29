df7516b4c601cd96ae20ff9fb090861c38d8f78d
Cristian Buitrago <cbo@misena.edu.co>
2022-03-29 06:15:31 -0500

    Header component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,16 @@
+import Header from "./componentes/Header"
+
 function App(){
-
-    const title = 'Blog Post'
-    const body = 'This is my blog post'
-    const comments = [
-        {id: 1 , text: 'First Comment'},
-        {id: 2 , text: 'Second Comment'},
-        {id: 3 , text: 'Third Comment'},
-    ]
-
-    const loading = false  
-    const showComments = true
-    const commentBlock = ( 
-        <div className="comments">
-                <h3>Comments ({comments.length})</h3>
-                <ul>
-                    { comments.map( (comment, index) => (
-                        <li key={index}>{ comment.text }</li>
-                    )) }
-                </ul>
-            </div>
-        )
-    
-    
-    if(loading) return <h1>Loading...</h1>
-
     return (
-        <div className="container">
-            <h1>{ title }</h1>
-            <p>{ body }</p>
-            {showComments && commentBlock }
-            
-        </div>
-       
+        <>
+            <Header />
+                <div className="container">
+                    <h1>My app</h1>   
+                </div>       
+        </>
+     
     )
+
 }
 
 export default App
