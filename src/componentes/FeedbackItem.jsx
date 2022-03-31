6babd3c7cfb57c9cacdc78aecfc31777e407bede
Cristian Buitrago <cbo@misena.edu.co>
2022-03-31 17:17:13 -0500

    useState Hook

--- src/componentes/FeedbackItem.jsx
+++ src/componentes/FeedbackItem.jsx
@@ -1,12 +1,25 @@
+import {useState} from 'react'
+
+
 function FeedbackItem() {
-  return (
+    const [rating, setRating] = useState(7)
+    const [text, setText] = useState('This is an example of a feedback item')
+  
+    const handleClick  = () =>{
+        setRating(10)
+    }
+
+    return (
     <div className="card">
         <div className="num-display">
-            10
+            {rating}
         </div>
         <div className="text-display">
-            This is an example of a feedback item
+            {text}
         </div>
+        <button onClick={ handleClick }>
+            Click
+        </button>
     </div>
   )
 }
