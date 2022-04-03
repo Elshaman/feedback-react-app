ce91488a3c2d76da63f1e03b413b3ab0be50ee29
Cristian Buitrago <cbo@misena.edu.co>
2022-04-02 19:13:53 -0500

    first card component

--- src/componentes/FeedbackItem.jsx
+++ src/componentes/FeedbackItem.jsx
@@ -1,14 +1,9 @@
-import {useState} from 'react'
-
+import Card from "./shared/Card"
 
 function FeedbackItem({item}) {
-    //const [rating, setRating] = useState(7)
-    //const [text, setText] = useState('This is an example of a feedback item')
-  
-  
-
+    
     return (
-    <div className="card">
+    <Card>
         <div className="num-display">
             {item.rating}
         </div>
@@ -16,7 +11,7 @@ function FeedbackItem({item}) {
             {item.text}
         </div>
       
-    </div>
+    </Card>
   )
 }
 
