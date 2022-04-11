6507c5ba4e8e5fe2e46fdea96aac0de9251a1fde
Cristian Buitrago <cbo@misena.edu.co>
2022-04-11 11:48:22 -0500

    redirect and navigate hook

--- src/componentes/Post.jsx
+++ src/componentes/Post.jsx
@@ -1,14 +1,26 @@
-import {useParams} from 'react-router-dom'
+import {Navigate, useNavigate, Routes , Route} from 'react-router-dom'
 
 
 function Post() {
 
-    const params = useParams()
+  const status = 200
+  const navigate = useNavigate()
+  if(status === 404){
+      return <Navigate to='/notfound' />
+  }
+
+  const onClick = () =>{
+      console.log('Hello')
+      navigate('/about')
+  }
 
   return (
       <>
-        <div>Post {params.id}</div>
-        <div>Post de {params.name}</div>
+        <div>Post</div>
+        <button onClick={onClick}> Click </button>
+        <Routes>
+            <Route path='/show' element={<h1>Hello</h1>} />
+        </Routes>
       </>
    
   )
